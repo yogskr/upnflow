2a3022fbe5e31af89f2cec2dfbd6999291f7feda
yogskr <yogakrisanta@gmail.com>
2024-06-08 19:49:24 +0700

    Added post cards or list of blog posts to homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
+import getPostMetadata from "@/utils/getPostMetadata";
 import styles from "./page.module.css";
+import PostCard from "@/components/post-card/PostCard";
 
 export default function Home() {
+  const postMetaData = getPostMetadata("blog-posts");
   return (
-    <>
-      <main className={styles.container}>
-        <h1>Homepage</h1>
-      </main>
-    </>
+    <main>
+      <div className={styles.container}>
+        {postMetaData.map((post, postIndex) => {
+          return <PostCard key={postIndex} post={post} />;
+        })}
+      </div>
+    </main>
   );
 }
