9f4c3d308934b29fa69b1ed72078df34e6f65470
yogskr <yogakrisanta@gmail.com>
2024-06-10 00:40:25 +0700

    Updated the file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx';
+import rehypeHighlight from 'rehype-highlight';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions`` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [rehypeHighlight],
+  },
+});
+
+// Wrap MDX and Next.js config with each other
+export default withMDX(nextConfig);
