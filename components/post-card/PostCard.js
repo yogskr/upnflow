2ffa3a43cfd09a5521050bc1f7bd7333e4de00bd
yogskr <yogakrisanta@gmail.com>
2024-07-22 01:49:36 +0700

    Modified the post card content

--- components/post-card/PostCard.js
+++ components/post-card/PostCard.js
@@ -1,4 +1,4 @@
-// components/post-card/PostCard.js
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from './PostCard.module.css';
 
@@ -6,8 +6,13 @@ export default function PostCard({ post }) {
   return (
     <section className={styles.card}>
       <h2 className={styles.title}>{post.meta.title}</h2>
-      <p className={styles.description}>{post.meta.description}</p>
-      <span className={styles.readMore}>
+      <Image
+        src={post.meta.image}
+        alt={post.meta.title}
+        width={530}
+        height={350}
+      />
+      <span className="readMore">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 448 512"
@@ -16,7 +21,7 @@ export default function PostCard({ post }) {
           fill="currentColor">
           <path d="M384 32c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H384zM160 144c-13.3 0-24 10.7-24 24s10.7 24 24 24h94.1L119 327c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l135-135V328c0 13.3 10.7 24 24 24s24-10.7 24-24V168c0-13.3-10.7-24-24-24H160z" />
         </svg>
-        <Link href={`/blogs/${post.slug}`} className={styles.readMore}>
+        <Link href={`/blogs/${post.slug}`} className="readMore">
           Read More
         </Link>
       </span>
