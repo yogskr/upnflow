afa7d7be97c24f02288e5689c163b4794fa9bebc
yogskr <yogakrisanta@gmail.com>
2024-07-22 01:49:10 +0700

    Modified the latest post content

--- components/latest-post/LatestPost.js
+++ components/latest-post/LatestPost.js
@@ -6,24 +6,29 @@ import PostInfo from '../post-info/PostInfo';
 
 export default function LatestPost({ post }) {
   return (
-    <section className={styles.postContainer}>
-      <Image
-        src={post.meta.image}
-        alt={post.meta.title}
-        width={600}
-        height={400}
-        className={styles.postImage}
-      />
-      <article className={styles.postDesc}>
-        <h2 className={styles.postTitle}>{post.meta.title}</h2>
-        <p>{post.meta.description}</p>
-        <PostInfo
-          author={post.meta.author}
-          date={post.meta.date}
-          showReadingTime={true}
-          readingTime={post.readingTime}
+    <div className={styles.postContainer}>
+      <h3 className={styles.latestPostTag}>Latest Post</h3>
+      <section className={styles.content}>
+        <Image
+          src={post.meta.image}
+          alt={post.meta.title}
+          width={600}
+          height={400}
+          className={styles.postImage}
         />
-      </article>
-    </section>
+        <article className={styles.postDesc}>
+          <h2 className={styles.postTitle}>
+            <Link href={`/blogs/${post.slug}`}>{post.meta.title}</Link>
+          </h2>
+          <p>{post.meta.description}</p>
+          <PostInfo
+            author={post.meta.author}
+            date={post.meta.date}
+            showReadingTime={false}
+            readingTime={post.readingTime}
+          />
+        </article>
+      </section>
+    </div>
   );
 }
