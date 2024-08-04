aac30e1827aa542d27d6b72245aaa124f0a0aef3
yogskr <yogakrisanta@gmail.com>
2024-08-04 22:44:47 +0700

    Modified the transition duration to 0.6s

--- app/template.js
+++ app/template.js
@@ -7,7 +7,7 @@ export default function Template({ children }) {
     <motion.div
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: 'easeInOut', duration: 0.9 }}>
+      transition={{ ease: 'easeInOut', duration: 0.6 }}>
       {children}
     </motion.div>
   );
